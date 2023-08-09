48abd612b46ad815b13b3e58629e01e9bf8b748c
dmtweedy <damon.tweedy@yahoo.com>
2023-08-09 10:44:46 -0500

    added script and database execution to index.js, added prompts to cli.js

--- cli.js
+++ cli.js
@@ -3,3 +3,53 @@ const departmentQueries = require('./models/departmentQueries');
 const roleQueries = require('./models/roleQueries');
 const employeeQueries = require('./models/employeeQueries');
 
+async function mainMenu() {
+  const { choice } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ]);
+
+    switch (choice) {
+      case 'View all departments':
+        // Call departments
+        break;
+      case 'View all roles':
+        // Call roles
+        break;
+      case 'View all employees':
+        // Call employees
+        break;
+      case 'Add a department':
+        // Call department
+        break;
+      case 'Add a role':
+        // Call role
+        break;
+      case 'Add an employee':
+        // Add employee
+        break;
+      case 'Update an employee role':
+        // Update employee role
+        break;
+      case 'Exit':
+        // Exit
+        return;
+    }
+  
+    mainMenu();
+  }
+
+  mainMenu();
--- index.js
+++ index.js
@@ -12,3 +12,37 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+// Read and execute scripts
+function executeSchemaScripts(connection) {
+  const schemaPath = path.join(__dirname, 'models');
+  const schemaFiles = ['departmentSchema.sql', 'employeeSchema.sql', 'roleSchema.sql'];
+
+  for (const file of schemaFiles) {
+    const script = fs.readFileSync(path.join(schemaPath, file), 'utf8');
+    connection.query(script, (err, results) => {
+      if (err) {
+        console.error(`Error executing ${file}:`, err);
+      } else {
+        console.log(`Executed ${file}`);
+      }
+    });
+  }
+}
+
+// Initialize database
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+
+  console.log('Connected to database');
+
+  // Execute scripts
+  executeSchemaScripts(connection);
+
+  // Release database when application exits
+  process.on('exit', () => {
+    connection.release();
+  });
+});
